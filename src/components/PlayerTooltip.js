72faa4572dff8dea119f16f6f57683469af4bcdf
Samuel Alvarez <seam.2194@gmail.com>
2018-09-21 17:03:40 -0400

    Fix z-index in player tooltips

--- src/components/PlayerTooltip.js
+++ src/components/PlayerTooltip.js
@@ -6,8 +6,10 @@ export default class PlayerTooltip extends Component {
   state = { isOpen: false };
   render() {
     return (
-      <div className={css.container} onMouseEnter={this.openTooltip} onMouseLeave={this.closeTooltip} style={this.props.position}>
-        <span>{this.props.number}</span>
+      <div className={css.container} style={this.props.position}>
+        <div className={css.players} onMouseEnter={this.openTooltip} onMouseLeave={this.closeTooltip} >
+          <span>{this.props.number}</span>
+        </div>
         <div className={this.state.isOpen ? css.activeArrow : css.defaultArrow} />
         <section className={this.state.isOpen ? css.activeContent : css.defaultContent}>
           {this.props.name}
