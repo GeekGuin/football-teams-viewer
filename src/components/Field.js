e4be5cec93f339d1547f748b244d08815b1109b1
Victor Tortolero <victor.tortolero@wizeline.com>
2018-09-21 16:24:54 -0500

    Revert players to original position in the JSX

--- src/components/Field.js
+++ src/components/Field.js
@@ -8,17 +8,17 @@ const Field = () => {
   return (
     <div className={css.default}>
       <img src={fieldImg} alt="Field" className={css.image}/>
-      <PlayerTooltip number='13' name='Alisson Becker' position={{top: '186px', left: '440px'}} />
-      <PlayerTooltip number='4' name='Virgil van Dijk' position={{top: '130px', left: '353px'}} />
-      <PlayerTooltip number='12' name='Joe Gomez' position={{top: '250px', left: '353px'}} />
-      <PlayerTooltip number='26' name='Andy Robertson' position={{top: '330px', left: '353px'}} />
-      <PlayerTooltip number='66' name='Treant Alexander-Arnold' position={{top: '30px', left: '353px'}} />
+      <PlayerTooltip number='10' name='Sadio Mane' position={{top: '300px', left: '100px'}} />
+      <PlayerTooltip number='11' name='Mohamed Salah' position={{top: '66px', left: '100px'}} />
+      <PlayerTooltip number='9' name='Roberto Firmino' position={{top: '186px', left: '45px'}} />
       <PlayerTooltip number='8' name='Naby Keita' position={{top: '186px', left: '231px'}} />
       <PlayerTooltip number='14' name='Jordan Henderson' position={{top: '66px', left: '203px'}} />
       <PlayerTooltip number='7' name='James Milner' position={{top: '300px', left: '203px'}} />
-      <PlayerTooltip number='11' name='Mohamed Salah' position={{top: '66px', left: '100px'}} />
-      <PlayerTooltip number='10' name='Sadio Mane' position={{top: '300px', left: '100px'}} />
-      <PlayerTooltip number='9' name='Roberto Firmino' position={{top: '186px', left: '45px'}} />
+      <PlayerTooltip number='26' name='Andy Robertson' position={{top: '330px', left: '353px'}} />
+      <PlayerTooltip number='66' name='Treant Alexander-Arnold' position={{top: '30px', left: '353px'}} />
+      <PlayerTooltip number='4' name='Virgil van Dijk' position={{top: '130px', left: '353px'}} />
+      <PlayerTooltip number='12' name='Joe Gomez' position={{top: '250px', left: '353px'}} />
+      <PlayerTooltip number='13' name='Alisson Becker' position={{top: '186px', left: '440px'}} />
     </div>
   );
 }
