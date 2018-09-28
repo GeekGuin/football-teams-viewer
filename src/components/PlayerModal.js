78dc27f7d8d4e8a02f1470e60d71700806ca1c66
Samuel Alvarez <seam.2194@gmail.com>
2018-09-27 23:18:00 -0400

    Add modal styles

--- src/components/PlayerModal.js
+++ src/components/PlayerModal.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import css from "./css/PlayerModal.css";
 
 export default class PlayerModal extends Component {
   render() {
     return (
-      <div className="default">
+      <div className={css.default} onClick={this.stopClosing}>
         <div>HORY SHET</div>
       </div>
     );
   }
+
+  stopClosing = e => {
+    console.log("no close!");
+    e.stopPropagation();
+  };
 }
